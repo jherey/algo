e284034be8095b2803fc94c6004e666c67ce3b0f
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2019-11-06 20:22:44 +0100

    binary search

---
+++ searching_algorithms/binarySearch.js
@@ -0,0 +1,20 @@
+function binarySearch(arr, elem) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  console.log(start, middle, end);
+  if (arr[middle] === elem) return middle;
+  return -1;
+}
+
+const a = binarySearch([2,5,6,9,13,15,28,30], 25);
+console.log(a)
